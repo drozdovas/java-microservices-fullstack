e3bf1a222d13354b093d57bd3d18f1ca3225e1a4
drozdovas <dmitrijus.drozdovas@gmail.com>
2025-01-20 10:14:27 +0000

    Added inventory-service to manage what we have in stock.

---
+++ inventory-service/src/main/java/com/drozdovas/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.drozdovas.microservices.inventory.repository;
+
+import com.drozdovas.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
